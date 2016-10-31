f2be93f0b7d298fd2a19e1478e67de82550692b3
pumpkin <zhenhuanlee@outlook.com>
2016-10-31 16:10:11 +0800

    devel

--- config/deploy.rb
+++ config/deploy.rb
@@ -25,13 +25,15 @@ set :bundle_flags, '--deployment'
 
 # Default branch is :master
 # ask :branch, `git rev-parse --abbrev-ref HEAD`.chomp
-set :branch, :master
+# set :branch, :master
 
 # Default deploy_to directory is /var/www/my_app_name
 set :deploy_to, '/home/jude/www/helloCap'
 
 # Default value for :scm is :git
 set :scm, :git
+
+set :branch, :devel
 
 # Default value for :format is :pretty
 # set :format, :pretty
@@ -71,8 +73,8 @@ namespace :deploy do
   desc "Make sure local git is in sync with remote."
   task :check_revision do
     on roles(:app) do
-      unless `git rev-parse HEAD` == `git rev-parse origin/master`
-        puts "WARNING: HEAD is not the same as origin/master"
+      unless `git rev-parse HEAD` == `git rev-parse origin/devel`
+        puts "WARNING: HEAD is not the same as origin/devel"
         puts "Run `git push` to sync changes."
         exit
       end
